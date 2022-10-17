c1abbfa80e5a28f7e424dfb0139e8ac3e373e3dd
Sean Cavalieri <sean@cavalieri.com>
2022-10-17 17:04:16 -0400

    Prepare command.ml

--- src/command.ml
+++ src/command.ml
@@ -1,5 +1,12 @@
 (* Parses the user input input *)
 
 (* Data type for a command *)
+type command =
+  | Deal
+  | AddPlayer of (String * Int) (* The name and their initial coins *)
+  | Leave
 
-(* Parse function *)
+(* Parse function String -> command *)
+(* E.g. "Deal" makes Deal
+   "AddPlayer Sean 100" makes AddPlayer holding tuple ("Sean", 100)
+   "Leave" makes leave *)
