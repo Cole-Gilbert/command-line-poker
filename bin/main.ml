5aed932334503b66c1600a545ae12902b2eb5e28
Mateo <maw346@cornell.edu>
2022-10-25 19:39:30 -0400

    Shorten bin/main.buyin and .get_input

--- bin/main.ml
+++ bin/main.ml
@@ -15,19 +15,14 @@ let validate st1 st2 =
 let rec get_input st =
   State.state_to_string st |> print_string;
   print_string "\n> ";
-  match read_line () with
-  | exception End_of_file ->
+  match Command.parse (read_line ()) with
+  | exception Command.Malformed ->
+      print_string "Malformed Command. Please try again.\n";
+      get_input st
+  | exception Command.Empty ->
       print_string "Please Enter a Command\n";
       get_input st
-  | input -> (
-      match Command.parse input with
-      | exception Command.Malformed ->
-          print_string "Malformed Command. Please try again.\n";
-          get_input st
-      | exception Command.Empty ->
-          print_string "Please Enter a Command\n";
-          get_input st
-      | cmd -> cmd)
+  | cmd -> cmd
 
 (** [game_loop st] is the new state after following a command, or the current
     state again if the command was Illegal *)
@@ -38,20 +33,15 @@ let rec game_loop st =
 
 (** [buyin ()] is a valid integer buy-in amount *)
 let rec buyin () =
-  match read_line () with
-  | exception End_of_file ->
+  match read_line () |> String.trim |> int_of_string with
+  | exception _ ->
       print_string "Please enter an integer.\n> ";
       buyin ()
-  | input -> (
-      match int_of_string (String.trim input) with
-      | exception Failure m ->
-          print_string "Please enter an integer.\n> ";
-          buyin ()
-      | i ->
-          if i <= 0 then (
-            print_string "Please enter a positive integer.\n> ";
-            buyin ())
-          else i)
+  | i ->
+      if i <= 0 then (
+        print_string "Please enter a positive integer.\n> ";
+        buyin ())
+      else i
 
 let main () =
   ANSITerminal.print_string [ ANSITerminal.blue ]
